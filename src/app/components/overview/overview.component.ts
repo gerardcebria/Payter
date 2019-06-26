d4148e66eee06956773dc9b49d1b4a972a3158cc
GCebria <gerardcebria@gmail.com>
2019-06-26 15:45:57 +0200

    Assignment 6 Filter

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -13,14 +13,17 @@ import { DataService } from 'app/services/data';
 export class OverviewComponent implements OnInit {
 
   $items: Subject<DataItem[]> = new BehaviorSubject<DataItem[]>([]);
-
   constructor(
     private dataService: DataService,
     private router: Router,
     private route: ActivatedRoute,
-  ) {}
+  ) { }
 
   ngOnInit() {
+    this.loadAllItems();
+  }
+
+  loadAllItems() {
     this.route.params.subscribe(params => {
       this.dataService.listItems().subscribe(items => {
         this.$items.next(items);
@@ -29,10 +32,37 @@ export class OverviewComponent implements OnInit {
   }
 
   onSelectItem(id: number) {
-    this.router.navigate(['/items','details', id]);
+    this.router.navigate(['/items', 'details', id]);
   }
 
   onNewItem() {
     this.router.navigate(['new']);
   }
+
+
+  searchFilter(searchValue) {
+    console.log(searchValue);
+    if (searchValue != "") {
+
+
+      //Parse the input into a number
+      var idNumber: number = +searchValue;
+
+      //Getting the item with the id
+      this.dataService.getItem(idNumber).subscribe(item => {
+
+        //Delete all items and show only the founded
+        var itemsaux = [];
+        itemsaux.push(item);
+        this.$items.next(itemsaux);
+
+      });
+
+    } else {
+      //If the input is empty then load all items
+      this.loadAllItems();
+    }
+  }
+
+
 }
